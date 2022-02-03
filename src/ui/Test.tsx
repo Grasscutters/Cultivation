9e9aff93e9520a3ed82e15ff7e65f111e95f010e
KingRainbow44 <kobedo11@gmail.com>
2022-02-03 14:00:00 +0100

    Update testing environment

--- src/ui/Test.tsx
+++ src/ui/Test.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import './App.css'
+
+import TopBar from './components/TopBar'
 
 import {invoke} from '@tauri-apps/api/tauri'
 import {dataDir} from '@tauri-apps/api/path'
@@ -11,12 +14,22 @@ async function stopProxy() {
   await invoke('disconnect')
 }
 
+async function generateCertificates() {
+  await invoke('generate_ca_files', { path: await dataDir() + '\\cultivation' })
+}
+
+function none() {
+  alert('none')
+}
+
 class Test extends React.Component<any, any>{
   render() {
     return (
       <div className="App">
+        <TopBar optFunc={none} downFunc={none} gameFunc={none} />
         <button onClick={startProxy}>start proxy</button>
         <button onClick={stopProxy}>stop proxy</button>
+        <button onClick={generateCertificates}>generate certificates</button>
       </div>
     )
   }
