8b51488a419bc6b7746b3d7fb62ab4008ad7a7db
KingRainbow44 <kobedo11@gmail.com>
2022-05-08 18:51:35 -0400

    Restructure code

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./utils/reportWebVitals";
+import App from "./ui/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
