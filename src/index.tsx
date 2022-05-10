d8323b09d5d8a8ca72f7ca1f80565bfb0d82c362
KingRainbow44 <kobedo11@gmail.com>
2022-05-10 18:50:58 -0400

    Using the testing component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import './index.css'
-import reportWebVitals from './utils/reportWebVitals'
 import App from './ui/App'
+// import Test from './ui/Test'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,4 +18,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+import reportWebVitals from './utils/reportWebVitals'
 reportWebVitals()
