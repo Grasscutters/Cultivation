d5b315d48abe4b83a854eee9e204aa3b38d75179
SpikeHD <spikegdofficial@gmail.com>
2022-05-14 00:27:09 -0700

    divider files

---
+++ src/ui/components/menu/Divider.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Divider.css'
+
+export default class Divider extends React.Component {
+  render() {
+    return (
+      <div className="Divider">
+        <div className="DividerLine"></div>
+      </div>
+    )
+  }
+}
