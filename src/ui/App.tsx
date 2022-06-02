b16104963ca9d89e1e8a463b3de6d523c5eb789f
KingRainbow44 <kobedo11@gmail.com>
2022-06-02 19:47:55 -0400

    Use the Grasscutter API to get the background file

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -45,7 +45,7 @@ class App extends React.Component<IProps, IState> {
       miniDownloadsOpen: false,
       downloadsOpen: false,
       gameDownloadsOpen: false,
-      bgFile: 'https://webstatic.hoyoverse.com/upload/event/2020/11/04/7fd661b5184e1734f91f628b6f89a31f_7367318474207189623.png',
+      bgFile: 'https://api.grasscutters.xyz/content/bgfile',
     }
 
     listen('lang_error', (payload) => {
