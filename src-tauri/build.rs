27a10c58cae80a1c210f2e321b3a353525d26ea4
Brian Bowman <seeker14491@gmail.com>
2022-07-18 18:00:46 -0500

    Fix lints

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -2,17 +2,14 @@ fn main() {
   cc::Build::new()
     .include("mhycrypto")
     .cpp(true)
-
     .file("mhycrypto/memecrypto.cpp")
     .file("mhycrypto/metadata.cpp")
     .file("mhycrypto/metadatastringdec.cpp")
-
     .compile("mhycrypto");
 
-    cc::Build::new()
+  cc::Build::new()
     .include("mhycrypto")
     .file("mhycrypto/aes.c")
-  
     .compile("mhycrypto-aes");
 
   tauri_build::build()
