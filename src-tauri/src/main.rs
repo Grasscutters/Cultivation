96d69d9ff6d21a12e4cacd57874cf57a836c22f6
SpikeHD <spikegdofficial@gmail.com>
2022-08-25 19:04:08 -0700

    bump initial wait for game to 10 seconds

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -94,7 +94,7 @@ fn enable_process_watcher(window: tauri::Window, process: String) {
 
   thread::spawn(move || {
     // Initial sleep for 8 seconds, since running 20 different injectors or whatever can take a while
-    std::thread::sleep(std::time::Duration::from_secs(8));
+    std::thread::sleep(std::time::Duration::from_secs(10));
 
     let mut system = System::new_all();
 
