249f07927d1c9b6fa75ed9e65c22e9025f1e9f5f
Thoronium <107363768+NotThorny@users.noreply.github.com>
2023-02-26 12:40:14 -0700

    Fix check

--- src-tauri/src/unzip.rs
+++ src-tauri/src/unzip.rs
@@ -97,7 +97,7 @@ pub fn unzip(
     }
 
     // If downloading full build, emit that the jar was extracted with it
-    if zipfile == "GrasscutterCulti3.4.zip" {
+    if zipfile.ends_with("Culti3.4.zip") {
       window
         .emit("jar_extracted", destpath.to_string() + "grasscutter.jar")
         .unwrap();
