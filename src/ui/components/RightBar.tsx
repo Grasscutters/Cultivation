9dcaea7e5b4d3573bd81a89e8059b98dbc0391fe
Wehi <notstrangequark@gmail.com>
2023-05-10 02:31:42 +0200

    Deleted the welcome message cause my friends said it sucks

--- src/ui/components/RightBar.tsx
+++ src/ui/components/RightBar.tsx
@@ -24,13 +24,7 @@ export default class RightBar extends React.Component {
           <div className="BarGithub BarImg" id="rightBarButtonGithub" onClick={() => this.openInBrowser(GITHUB)}>
             <img src={Github} />
           </div>
-          <div>
-            <div className="WelcomeDiv">
-            <span className='WelcomeMessage magic-text'>Welcome to Cultivation!</span>
 
-            </div>
-
-          </div>
         </div>
       </div>
     )
