f1ecb1aab0a6a63c6590364ff2b87baee1ec925d
Thoronium <107363768+NotThorny@users.noreply.github.com>
2023-06-25 13:36:01 -0600

    Fix handling

--- src/ui/components/menu/Options.tsx
+++ src/ui/components/menu/Options.tsx
@@ -152,7 +152,9 @@ export default class Options extends React.Component<IProps, IState> {
 
     // If setting any other game, automatically set to redirect more
     if (!value.toLowerCase().includes('genshin' || 'yuanshen')) {
-      this.toggleOption('redirect_more')
+      if (!this.state.redirect_more) {
+        this.toggleOption('redirect_more')
+      }
     }
 
     this.setState({
