91cb38c83794fb0b780abb3173d926059abd3972
KingRainbow44 <kobedo11@gmail.com>
2023-11-27 12:14:22 -0500

    (chore) Add support for `.scss` and `tailwindcss`

---
+++ cfg/tailwind.config.ts
@@ -0,0 +1,11 @@
+import { Config } from "tailwindcss";
+
+export default {
+    content: ["./src/**/*.{html,js,tsx,ts}"],
+    mode: "jit",
+    theme: {
+        extend: {}
+    },
+    darkMode: "class",
+    plugins: []
+} as Config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import postcss from "./cfg/postcss.config";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [preact()],
+    plugins: [
+        preact(),
+        tsconfigPaths()
+    ],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-  }
+    css: { postcss },
+
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    //
+    // 1. prevent vite from obscuring rust errors
+    clearScreen: false,
+    // 2. tauri expects a fixed port, fail if that port is not available
+    server: {
+        port: 1420,
+        strictPort: true,
+    }
 }));
