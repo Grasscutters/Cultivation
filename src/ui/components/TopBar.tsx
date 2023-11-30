6a7097eae1eaed85408651951e8e96c77481f7c7
KingRainbow44 <kobedo11@gmail.com>
2023-11-29 22:16:49 -0500

    (feat:layout) Add a proper window top bar

--- src/ui/components/TopBar.tsx
+++ src/ui/components/TopBar.tsx
@@ -1,7 +1,16 @@
+import "@css/components/TopBar.scss";
+
 function TopBar() {
     return (
-        <div>
+        <div class={"TopBar"} data-tauri-drag-region>
+            <div class={"flex flex-row gap-1 text-white"}>
+                <p>Cultivation</p>
+                <p>2.0.0</p>
+            </div>
 
+            <div>
+
+            </div>
         </div>
     );
 }
