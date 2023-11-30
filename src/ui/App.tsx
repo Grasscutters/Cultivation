a0206b0cc54b0efafe33c235b01e6405bb64f149
KingRainbow44 <kobedo11@gmail.com>
2023-11-29 23:50:48 -0500

    (feat:background) Add the background to the launcher's main screen

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -4,13 +4,21 @@ import Router, { Route } from "preact-router";
 import TopBar from "@components/TopBar.tsx";
 import Launcher from "@ui/layout/Launcher.tsx";
 
+import { useBackground } from "@backend/stores/mihoyo.ts";
+
 import { PageRoutes } from "@app/constants.ts";
 
 import "@css/App.scss";
 
 function App() {
+    const background = useBackground();
+
     return (
-        <div class={"App"}>
+        <div class={"App"}
+             style={{
+                 backgroundImage: `url(${background})`
+             }}
+        >
             <TopBar />
 
             <Router>
